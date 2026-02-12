997d0f655f56274988b3be20512a09efb7a42dca
codewithayana <codewithayana@gmail.com>
2026-02-12 12:05:34 +0530

    feat: add portfolio data constants and associated image assets

--- src/components/constants/data.ts
+++ src/components/constants/data.ts
@@ -10,8 +10,8 @@ export const ABOUT_ME = {
     "Hey! I’m Ayana Dinesh, a Full Stack Developer who enjoys building secure and scalable backend systems. I like breaking down complex problems and turning ideas into clean, efficient APIs and applications.",
     "I’m especially interested in secure authentication, RESTful API design, and data-driven solutions. I aim to build backend systems that are not just functional, but also maintainable, modern, and ready to scale.",
   ],
-  profileImage: "/sana 1.jpeg", // Restore original image
-  profileGif: "/profile-gif.gif", // Replace with your personalised gif
+  profileImage: "/ayana 1 .jpg", // Restore original image
+  profileGif: "/200.webp", // Replace with your personalised gif
 };
 
 export const USER_NAMES = {
@@ -73,15 +73,15 @@ export const SKILLS = {
 export const PROJECTS = [
   {
     id: 1,
-    name: "node-rest-api-typescript",
+    name: "node-rest-api-ts",
     category: "BACKEND",
     description:
       "**Problem:** Traditional APIs often suffer from performance bottlenecks and weak type safety.\n**Impact:** This leads to fragile codebases and poor developer experience.\n**Solution:** A scalable Node.js REST API built with TypeScript, featuring JWT-based authentication, MongoDB for data persistence, and Redis for caching. Dockerized for consistent deployment.",
     tech: ["Node.js", "TypeScript", "JWT", "Redis", "Docker", "MongoDB"],
     link: "",
     github: "https://github.com/codewithayana/node_rest-api-ts",
-    image: "https://images.unsplash.com/photo-1555066931-4365d14bab8c?q=80&w=2070&auto=format&fit=crop",
-    color: "#3178C6",
+    image: "https://imgs.search.brave.com/ZbqpwMOh6F6e3L5FqI-3reoSx6m2WuPxMHbzzVQSt-A/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA1LzQxLzY5Lzgy/LzM2MF9GXzU0MTY5/ODI3MV90cVNpYkxi/SjJpUGhjTjhockR5/OWNGRGpiZTk4Slli/US5qcGc",
+    color: "#00FF94",
   },
   {
     id: 2,
@@ -92,8 +92,8 @@ export const PROJECTS = [
     tech: ["Node.js", "JavaScript", "Bootstrap 5", "Handlebars", "Express.js", "MongoDB"],
     link: "https://lumina-skin.onrender.com/",
     github: "https://github.com/codewithayana/Lumina-Skin-ecommerce",
-    image: "https://images.unsplash.com/photo-1541692641319-981cc79ee10a?q=80&w=2070&auto=format&fit=crop",
-    color: "#00FF94",
+    image: "https://imgs.search.brave.com/7AubXD1cewzWJuzvnz9mLDABvx5y-rD5A4ya8O3gx70/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMucGV4ZWxzLmNv/bS9waG90b3MvMzc2/Mjg3Ny9wZXhlbHMt/cGhvdG8tMzc2Mjg3/Ny5qcGVnP2F1dG89/Y29tcHJlc3MmY3M9/dGlueXNyZ2ImZHBy/PTEmdz01MDA",
+    color: "hsla(26, 54%, 98%, 1.00)",
   },
 ];
 
